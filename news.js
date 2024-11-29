5c85b085e6314a26ff86d6b71b653a2978a8cb27
sStudent <junaidashraf2500@gmail.com>
2024-11-29 16:45:01 +0530

    News web

--- news.js
+++ news.js
@@ -7,6 +7,22 @@ window.addEventListener("load", ()=> fetchNews("India"));
 async function fetchNews(query) {
     const response = await fetch(`${url}${query}&apiKey=${API_KEY}`);
     const data = await response.json();
-    console.log(data);
+    bindData(data.articles);
+
     
+}
+
+function bindData(articles){
+    const cardsContainer = document.getElementById('cards-container');
+    const newsCardTemplate = document.getElementById('template-news-card')
+
+    cardsContainer.innerHTML='';
+
+articles.forEach(article => {
+    if(!article.urlToImage) return;
+    const cardClone = newsCardTemplate.content.cloneNode(true);
+    cardsContainer.appendChild(cardClone);
+         });
+     
+
 }
